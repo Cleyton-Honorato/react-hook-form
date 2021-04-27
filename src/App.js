fcdb4dd9babfc9e4c24277f3527aa4d5af783e74
Cleyton Honorato <honoraro55@gmail.com>
2021-04-27 14:56:15 -0300

    rising changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Input, message, Button } from "antd";
+import * as yup from "yup";
+
+import "./App.css";
+import "antd/dist/antd.css";
 
 function App() {
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .required(() => message.error("O email não pode ser vazio"))
+      .email(() => message.warning("Digite um email válido")),
+    password: yup
+      .string()
+      .required(() => message.error("A senha não pode ser vazia"))
+      .min(6, () => message.warning("A senha deve conter pelo menos 6 dígitos")),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    setValue,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  useEffect(() => {
+    register("email");
+    register("password");
+  }, [register]);
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="form">
+        <label className="label">Email</label>
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <Input
+              placeholder="email"
+              onChange={(el) => setValue("email", el.target.value)}
+            />
+          )}
+        />
+
+        <label className="label">Senha</label>
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <Input
+              placeholder="senha"
+              onChange={(el) => setValue("password", el.target.value)}
+            />
+          )}
+        />
+
+        <Button
+          className="submit"
+          onChange={() => handleSubmit(onSubmit)}
         >
-          Learn React
-        </a>
-      </header>
+          enviar
+        </Button>
+        
+      </div>
     </div>
   );
 }
